080decab92b36984916156794eb4c7b0547c03ca
Peter McCracken <peter@petermccracken.com>
2012-02-05 17:44:38 -0500

    using redis-url package

--- web.js
+++ web.js
@@ -1,14 +1,11 @@
 var express = require('express');
 var _ = require('underscore')._;
-var redis_lib = require('redis');
 var LineSet = require('./lineset').LineSet;
-var url = require('url');
 var RedisStore = require('connect-redis')(express);
 
 var redis;
 function setup_redis(redis_url) {
-  var parsed_url = url.parse(redis_url);
-  redis = redis_lib.createClient(parsed_url.port, parsed_url.hostname);
+  redis = require('redis-url').createClient(redis_url);
 }
 
 function current_line_set(session) {
@@ -27,7 +24,7 @@ app.use(express.methodOverride());
 app.use(express.cookieParser());
 
 app.configure('development', function() {
-  setup_redis('http://localhost:6379');
+  setup_redis('redis://localhost:6379');
 
   app.use(express.static(__dirname + '/public'));
   app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
@@ -37,19 +34,19 @@ app.configure('development', function() {
     store: new RedisStore({client: redis})
   }));
 
-  process.env['ARTSY_CURRENT_THICKNESS'] = 0.7
-  process.env['ARTSY_OLD_THICKNESS'] = 0.3
-  process.env['ARTSY_THICKNESS_DECAY'] = 0.0
+  process.env.ARTSY_CURRENT_THICKNESS = 0.7
+  process.env.ARTSY_OLD_THICKNESS = 0.3
+  process.env.ARTSY_THICKNESS_DECAY = 0.0
 });
 
 app.configure('production', function() {
-  setup_redis(process.env['REDISTOGO_URL']);
+  setup_redis(process.env.REDISTOGO_URL);
 
   var oneYear = 365 * 24 * 3600 * 1000; // milliseconds
   app.use(express.static(__dirname + '/public', { maxAge: oneYear }));
   app.use(express.errorHandler());
   app.use(express.session({
-    secret: process.env['ARTSY_SESSION_SECRET'],
+    secret: process.env.ARTSY_SESSION_SECRET,
     store: new RedisStore({client: redis})
   }));
 });
@@ -76,18 +73,18 @@ app.get('/config', function(req, res) {
 });
 
 app.put('/config', function(req, res) {
-  process.env['ARTSY_CURRENT_THICKNESS'] = parseFloat(req.body.current_thickness) || 0;
-  process.env['ARTSY_OLD_THICKNESS'] = parseFloat(req.body.old_thickness) || 0;
-  process.env['ARTSY_THICKNESS_DECAY'] = parseFloat(req.body.decay) || 0;
+  process.env.ARTSY_CURRENT_THICKNESS = parseFloat(req.body.current_thickness) || 0;
+  process.env.ARTSY_OLD_THICKNESS = parseFloat(req.body.old_thickness) || 0;
+  process.env.ARTSY_THICKNESS_DECAY = parseFloat(req.body.decay) || 0;
   res.redirect('/config');
 });
 
 app.get('/config.json', function(req, res) {
   res.contentType('json');
   res.send({
-    current_thickness: process.env['ARTSY_CURRENT_THICKNESS'],
-    old_thickness: process.env['ARTSY_OLD_THICKNESS'],
-    decay: process.env['ARTSY_THICKNESS_DECAY']
+    current_thickness: process.env.ARTSY_CURRENT_THICKNESS,
+    old_thickness: process.env.ARTSY_OLD_THICKNESS,
+    decay: process.env.ARTSY_THICKNESS_DECAY
   });
 });
 
